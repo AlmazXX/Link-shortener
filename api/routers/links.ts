25c9c1727e7d86b788c44e692c67e1c57827c85d
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 00:39:41 +0600

    api: Added reqProcessing of post() in links.ts

--- api/routers/links.ts
+++ api/routers/links.ts
@@ -1,12 +1,33 @@
 import {Router} from "express";
+import Link from "../models/Link";
+import {shortener} from "../helpers";
+import {ILink} from '../types'
+
 
 const linksRouter = Router();
 
-linksRouter.post('/', (req, res) => {
-    res.send('Link will be shortened here')
+linksRouter.post("/", async (req, res) => {
+    try {
+        console.log(req.body)
+        const linkData: ILink = {
+            shortUrl: shortener().getLink(),
+            originalUrl: req.body.url,
+        };
+        const link = new Link(linkData);
+
+        try {
+            await link.save();
+            return res.send(link);
+        } catch (e) {
+            return res.status(400).send(e)
+        }
+    } catch {
+        return res.sendStatus(500)
+    }
 })
 
-linksRouter.get('/:shortUrl', (req, res) => {
+linksRouter.get('/:shortUrl', async (req, res) => {
+    const links = await Link.findById(req.params.shortUrl)
     res.send('Short link will be given here')
 })
 
