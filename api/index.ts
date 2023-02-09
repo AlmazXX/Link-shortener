6cc0f1b3b22363a8093e373d3219718caff841ca
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-09 21:19:48 +0600

    api: Launched mongoose

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import linksRouter from "./routers/links";
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 8000;
@@ -11,9 +12,14 @@ app.use(express.json());
 app.use('/links', linksRouter)
 
 const run = async () => {
+  mongoose.set('strictQuery', false)
+  await mongoose.connect('mongodb://localhost/links')
   app.listen(port, () => {
     console.log("We are live on port: " + port);
   });
+  process.on('exit', () =>{
+    mongoose.disconnect()
+  })
 };
 
 run().catch(console.error);
