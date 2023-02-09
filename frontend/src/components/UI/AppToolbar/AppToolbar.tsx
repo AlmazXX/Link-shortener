dabecc8b8cf480adce0921e02edb6188237be9c1
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 01:19:55 +0600

    fEnd: Changed AppToolbar

--- frontend/src/components/UI/AppToolbar/AppToolbar.tsx
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -1,20 +1,12 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
-import { Link as NavLink } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
-const Link = styled(NavLink)({
-  color: "inherit",
-  textDecoration: "none",
-  "&:hover": {
-    color: "inherit",
-  },
-});
 
 const AppToolbar = () => {
   return (
     <AppBar position="sticky" sx={{ mb: 2 }}>
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to="/">CompStore</Link>
+          <a href='/' style={{textDecoration: 'none', color: 'inherit'}} >CompStore</a>
         </Typography>
       </Toolbar>
     </AppBar>
