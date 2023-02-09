7c28078463554ce75ba07282205267683f80a945
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 00:34:59 +0600

    api: Created shortener() helper

---
+++ api/helpers.ts
@@ -0,0 +1,14 @@
+ export const shortener = () => {
+    const abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const randIndex = () => Math.floor(Math.random() * abc.length);
+
+    return {
+        getLink() {
+            let randString = '';
+            for (let i = 0; i < 7; ++i) {
+                randString += abc[randIndex()];
+            }
+            return randString;
+        }
+    }
+}
