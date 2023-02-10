3e55622d6fcd11414210930d22560d6259900dbf
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 14:33:54 +0600

    fEnd: Changed arg type of createLink()

--- frontend/src/features/link/linkThunk.ts
+++ frontend/src/features/link/linkThunk.ts
@@ -1,7 +1,8 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axiosApi from "../../axiosApi";
+import {ILink} from "../../types";
 
-export const createLink = createAsyncThunk<string, string>('link/create', async (url) => {
-    const {data} = await axiosApi.post('/links', url);
+export const createLink = createAsyncThunk<string, ILink>('link/create', async (link) => {
+    const {data} = await axiosApi.post('/links', link);
     return data.shortUrl;
 });
