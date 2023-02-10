6f0023440b6b57f842380258921ebfc1cff7e96b
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 14:36:29 +0600

    fEnd: Added onSubmit() to Link.tsx

--- frontend/src/features/link/Link.tsx
+++ frontend/src/features/link/Link.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
 import {Grid, Typography} from "@mui/material";
 import LinkForm from "./LinkForm";
+import {useAppDispatch} from "../../app/hooks";
+import {createLink} from "./linkThunk";
+import {ILink} from "../../types";
+import LinkResult from "./LinkResult";
 
 const Link = () => {
+    const dispatch = useAppDispatch();
+
+    const onSubmit = (link: ILink) => {
+        dispatch(createLink(link))
+    }
+
     return (
-        <Grid container direction="column" spacing={2}>
+        <Grid container direction="column" spacing={2} alignItems='center'>
             <Grid item>
                 <Typography variant="h4">Shorten your link!</Typography>
             </Grid>
             <Grid item>
-                <LinkForm/>
+                <LinkForm onSubmit={onSubmit} />
+            </Grid>
+            <Grid item>
+                <LinkResult/>
             </Grid>
         </Grid>
     );
