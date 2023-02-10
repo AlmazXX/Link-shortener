4cd0da7a3144b67cf734e93029c92f0fdd725437
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 14:35:26 +0600

    fEnd: Created LinkResult

---
+++ frontend/src/features/link/LinkResult.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Grid, Typography} from "@mui/material";
+import {useAppSelector} from "../../app/hooks";
+import {selectShortUrl} from "./linkSlicer";
+import {apiURL} from "../../constants";
+
+const LinkResult = () => {
+    const shortUrl = useAppSelector(selectShortUrl);
+
+    return shortUrl ? (
+        <Grid container direction='column' spacing={2} alignItems='center'>
+            <Grid item>
+                <Typography variant="h6" component="div">Your new link look like this:</Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant='body2' component='a' color='#000' href={apiURL + '/' + shortUrl} target='_blank' style={{border: '1px solid rgba(0, 0, 0, 0.23)', borderRadius: '4px',padding: '16.5px 14px'}}>{apiURL + '/' + shortUrl}</Typography>
+            </Grid>
+        </Grid>
+    ) : null;
+};
+
+export default LinkResult;
