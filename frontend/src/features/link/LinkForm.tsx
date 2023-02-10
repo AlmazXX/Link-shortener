f545a2eeffbc73d7b8f49d4dc6a5e234c23377bc
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-10 14:35:04 +0600

    fEnd: Added onChange(), onFormSubmit() to LinkForm

--- frontend/src/features/link/LinkForm.tsx
+++ frontend/src/features/link/LinkForm.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import {ChangeEvent, FC, FormEvent, useState} from 'react';
 import {Button, Grid, TextField} from "@mui/material";
+import {useAppSelector} from "../../app/hooks";
+import {selectLinkCreating} from "./linkSlicer";
+import {ILink} from "../../types";
 
-const LinkForm = () => {
+interface Props {
+    onSubmit: (link: ILink) => void
+}
+const LinkForm: FC<Props> = ({onSubmit}) => {
+    const creating = useAppSelector(selectLinkCreating);
+    const [state, setState] = useState<ILink>({url: ''});
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setState(prev => ({...prev, [name]: value}))
+    }
+    const onFormSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        await onSubmit(state);
+        setState({url: ''})
+    }
+
     return (
-        <form>
-            <Grid container direction="column" spacing={2}>
-                <Grid item xs>
-                    <TextField id='link' name='link' label='Link'/>
+        <form autoComplete='off' onSubmit={onFormSubmit}>
+            <Grid container direction="column" spacing={2} alignItems='center'>
+                <Grid item>
+                    <TextField type='url' id='url' name='url' label='Link' required onChange={onChange} value={state.url} />
                 </Grid>
                 <Grid item xs>
-                    <Button type='submit' variant='contained' color="primary">Shorten!</Button>
+                    <Button type='submit' variant='contained' color="primary" disabled={creating === 'pending'}>Shorten!</Button>
                 </Grid>
             </Grid>
         </form>
