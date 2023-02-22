aa42783278d420bc05e24e344e0a41ef90848f5a
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-02-22 20:54:50 +0600

    refactor: changed component to display short link

--- frontend/src/features/link/LinkResult.tsx
+++ frontend/src/features/link/LinkResult.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import {Grid, Typography} from "@mui/material";
-import {useAppSelector} from "../../app/hooks";
-import {selectShortUrl} from "./linkSlicer";
-import {apiURL} from "../../constants";
+import { Grid, Link, styled, Typography } from "@mui/material";
+import { useAppSelector } from "../../app/hooks";
+import { apiURL } from "../../constants";
+import { selectShortUrl } from "./linkSlicer";
 
 const LinkResult = () => {
   const shortUrl = useAppSelector(selectShortUrl);
 
+  const ResultBox = styled(Link)({
+    border: "1px solid rgba(0, 0, 0, 0.23)",
+    borderRadius: "4px",
+    padding: "16.5px 14px",
+  });
+
   return shortUrl ? (
-    <Grid container direction='column' spacing={2} alignItems='center'>
+    <Grid container direction="column" spacing={2} alignItems="center">
       <Grid item>
-        <Typography variant="h6" component="div">Your new link look like this:</Typography>
+        <Typography variant="h6" component="div">
+          Your new link look like this:
+        </Typography>
       </Grid>
       <Grid item>
-        <Typography variant='body2' component='a' color='#000' href={apiURL + '/' + shortUrl} target='_blank' style={{
-          border: '1px solid rgba(0, 0, 0, 0.23)',
-          borderRadius: '4px',
-          padding: '16.5px 14px'
-        }}>{apiURL + '/' + shortUrl}</Typography>
+        <ResultBox
+          underline="none"
+          href={apiURL + "/" + shortUrl}
+          target="_blank"
+        >
+          {apiURL + "/" + shortUrl}
+        </ResultBox>
       </Grid>
     </Grid>
   ) : null;
